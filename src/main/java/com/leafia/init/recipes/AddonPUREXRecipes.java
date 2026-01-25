93f67a180b4174dbca3fcb5a5317a587152dd163
david <119733331+davidctinescu@users.noreply.github.com>
2026-01-25 15:49:19 +0200

    woahPower

---
+++ src/main/java/com/leafia/init/recipes/AddonPUREXRecipes.java
@@ -0,0 +1,36 @@
+package com.leafia.init.recipes;
+
+import com.hbm.inventory.RecipesCommon;
+import com.hbm.inventory.fluid.FluidStack;
+import com.hbm.inventory.fluid.Fluids;
+import com.hbm.inventory.recipes.PUREXRecipes;
+import com.hbm.inventory.recipes.loader.GenericRecipe;
+import com.hbm.items.ModItems;
+import com.leafia.contents.AddonFluids;
+import com.leafia.contents.AddonItems;
+import net.minecraft.item.ItemStack;
+
+public class AddonPUREXRecipes {
+    public static final PUREXRecipes INSTANCE = PUREXRecipes.INSTANCE;
+    public static void register() {
+        long woahPower = 1000L;
+        String autoWoah = "autoswitch.woah";
+        INSTANCE.register(
+                (new GenericRecipe("purex.zirnoxnu"))
+                        .setup(100,woahPower)
+                        .setNameWrapper("purex.recycle").setGroup(autoWoah, INSTANCE)
+                        .inputItems(new RecipesCommon.AStack[]{new RecipesCommon.ComparableStack(ModItems.powder_fertilizer)})
+                        .inputFluids(new FluidStack[]{
+                                new FluidStack(Fluids.WATER, 450),
+                                new FluidStack(AddonFluids.N2O, 550)
+                        })
+                        .outputItems(new ItemStack[]{
+                                new ItemStack(AddonItems.anitrate_solid, 1),
+                                new ItemStack(ModItems.nothing, 2),
+                                new ItemStack(ModItems.nothing, 1),
+                                new ItemStack(ModItems.nothing, 2)
+                        })
+                        .setIconToFirstIngredient()
+        );
+    }
+}
